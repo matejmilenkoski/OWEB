3577d0d9983a2b3ec41f613ea55d86e31f3e529f
matejmilenkoski <matejmilenkoski@gmail.com>
2024-11-26 06:12:04 +0100

    Add files via upload

---
+++ Domashna_3(so golemo zakasnuvanje)/word-list.js
@@ -0,0 +1,33 @@
+const wordList=[
+
+    {
+        word: "apple",
+    },
+    {
+        word: "house",
+    },
+    {
+        word: "train",
+    },
+    {
+        word: "smile",
+    },
+    {
+        word: "dream",
+    },
+    {
+        word: "shore",
+    },
+    {
+        word: "bread",
+    },
+    {
+        word: "frost",
+    },
+    {
+        word: "bloom",
+    },
+    {
+        word: "peace",
+    }
+];
